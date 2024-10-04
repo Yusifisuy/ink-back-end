955815f28957841a9d585a96b7c59b15c376c440
Zəhməti Yusif <110719808+Yusifisuy@users.noreply.github.com>
2024-10-03 22:27:40 -0700

    Add files via upload

---
+++ ink-back-end/src/main/java/az/inkstudio/backend/InkBackEndApplication.java
@@ -0,0 +1,13 @@
+package az.inkstudio.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InkBackEndApplication.class, args);
+    }
+
+}
---
+++ ink-back-end/src/test/java/az/inkstudio/backend/InkBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package az.inkstudio.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
